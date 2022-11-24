cd1725067c1f8a7b44addcedd545bf9bb9e0f3b4
BraianBecha <braian.becha.hace@gmail.com>
2022-11-24 02:29:47 -0300

    hice get

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
-import { NgModule } from '@angular/core';
+import { Component, importProvidersFrom, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortadaComponent } from './component/portada/portada.component';
+import { ProyectoComponent } from './component/proyecto/proyecto.component';
+import { IniciarSesionComponent } from './component/iniciar-sesion/iniciar-sesion.component';
+import { NotFoundComponentComponent } from './component/not-found-component/not-found-component.component';
+import { EducacionComponent } from './component/educacion/educacion.component';
+import { ExperienciaComponent } from './component/experiencia/experiencia.component';
 
-const routes: Routes = [];
+
+
+const appRoutes: Routes = [
+  { path: 'portada', component: PortadaComponent },
+  { path: 'proyectos', component: ProyectoComponent },  
+  { path: 'educacion', component: EducacionComponent },
+  { path: 'experiencia', component: ExperienciaComponent },  
+  {path:'iniciar-sesion', component: IniciarSesionComponent},
+  {path: '', component: IniciarSesionComponent,pathMatch:'full'},
+  { path: '**', component:NotFoundComponentComponent} 
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  
+  
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+
   exports: [RouterModule]
+
+
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortadaComponent } from './component/portada/portada.component';
+import { ExperienciaComponent } from './component/experiencia/experiencia.component';
+
+import { EducacionComponent } from './component/educacion/educacion.component';
+
+import { ProyectoComponent } from './component/proyecto/proyecto.component';
+
+import { ToolbarComponent } from './component/toolbar/toolbar.component';
+import { IniciarSesionComponent } from './component/iniciar-sesion/iniciar-sesion.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { BannerComponent } from './component/banner/banner.component';
+import { PieComponent } from './component/pie/pie.component';
+import{HttpClient, HttpClientModule} from '@angular/common/http';
+import { ProyectoServService } from './servicios/proyecto-serv.service';
+import { NotFoundComponentComponent } from './component/not-found-component/not-found-component.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortadaComponent,
+    ExperienciaComponent,
+    IniciarSesionComponent,
+    EducacionComponent,
+    ProyectoComponent,
+    NotFoundComponentComponent,
+    ToolbarComponent,
+    BannerComponent,
+    PieComponent
   ],
   imports: [
+    
+    ReactiveFormsModule,
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+
+  
+
+  providers: [ProyectoServService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
